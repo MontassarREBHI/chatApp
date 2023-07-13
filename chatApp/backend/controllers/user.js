a226498c98c097b1ce0d9ef7bd2f93a81d5f7ffb
unknown <oussemabenbouali.bouyahia@gmail.com>
2023-07-13 12:19:46 +0100

    users registred in db

---
+++ chatApp/backend/controllers/user.js
@@ -0,0 +1,21 @@
+const User = require("../models/user");
+
+const createUser = async (req, res) => {
+  const { username, socketId } = req.body;
+  const user = await User.findOne({ username });
+  if (user) return res.status(200).send("user already exisit");
+  else {
+    const newUser = new User({ username, socketId,connected:true });
+
+    newUser
+      .save()
+      .then(() =>
+        res.status(200).json({ newUser, message: "user created successfully!" })
+      )
+      .catch((err) => {
+        res.status(400).send("something wrong");
+      });
+  }
+};
+const allUsers= User.find({})
+module.exports={createUser,allUsers}
--- chatApp/backend/index.js
+++ chatApp/backend/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const connectDB= require('./db')
+const userController =require('./controllers/user')
+const User= require('./models/user')
 const app = express();
 connectDB();
 const http = require("http");
@@ -15,27 +17,35 @@ const io = new Server(server, {
 });
 
 app.use(express.json());
-let users = [];
-// app.get('/', (req, res) => {
-//     res.send('<h1>Hello world</h1>');
-//   });
-io.on("connection", (socket) => {
-  console.log("⚡ a user connected", socket.id);
+// let users = [];
 
+app.post('/login',userController.createUser)
+io.on("connection", async(socket) => {
+  console.log("⚡ a user connected", socket.id);
+  let users= await User.find({})
+  
+  //message event
   socket.on("message", (data) => {
     data.socketReceiver
       ? io.to(data.socketReceiver).emit("messageResponse", data)
       : io.emit("messageResponse", data);
   });
+
+  // newUser event
   socket.on("newUser", (data) => {
-    users.push(data);
+    
     io.emit("newUserResponse", users);
   });
+
+
+  // onTyping event
   socket.on("typing", (data) => socket.broadcast.emit("typingResponse", data));
+  
+  // disconnect event
   socket.on("disconnect", () => {
     console.log("🔥: A user disconnected");
     //Updates the list of users when a user disconnects from the server
-    users = users.filter((user) => user.socketID !== socket.id);
+    users = users.filter((user) => user.connected === true);
     // console.log(users);
     //Sends the list of users to the client
     io.emit("newUserResponse", users);
